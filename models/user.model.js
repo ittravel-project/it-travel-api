c3084aeb5ec10dd4ad6cc6b1cacb6293973753de
RedBalloon24 <worldtraveler757@gmail.com>
2019-06-22 14:54:00 +0200

    create model, routes, controllers

---
+++ models/user.model.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const constants = require('../constants');
+const SALT_WORK_FACTOR = 10;
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const PASSWORD_PATTERN = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+const URL_PATTERN = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        minlength: [3, 'Name needs at least 3 characters'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email is required',
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [EMAIL_PATTERN, 'Invalid email pattern']
+    },
+    password: {
+        type: String,
+        required: 'Password is required',
+        match: [PASSWORD_PATTERN, 'Passwords must contain at least six characters, including uppercase, lowercase letters and numbers.']
+    },
+    city: {
+        type: String,
+        required: 'City is required',
+    },
+    description: {
+        type: String
+    },
+    social: {
+        googleId: {
+          type: String
+        },
+    },
+    avatarURL: {
+    type: String,
+    match: [URL_PATTERN, 'Invalid avatar URL pattern']
+    }
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: (doc, ret) => {
+            ret.id = doc._id;
+            delete ret._id;
+            delete ret.__v;
+            delete ret.password;
+            return ret;
+        }
+    }
+});
+
+userSchema.pre('save', function (next) {
+const user = this;
+
+if (!user.isModified('password')) {
+next();
+} else {
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+        .then(salt => {
+        return bcrypt.hash(user.password, salt)
+            .then(hash => {
+            user.password = hash;
+            next();
+            })
+        })
+        .catch(next)
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = USer;
---
+++ routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../controllers');
+const secure = require('../middlewares/secure.mid');
+const uploader = require('../configs/storage.config');
+
+router.post('/register', auth.register);
+router.post('/autenthicate', auth.authenticate);
+router.get('/logout', auth.register);
+
+
+module.exports = router
